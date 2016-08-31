bacda0f703c6a5d97811616498cff3be1d650034
David Petro <david.abraao.petro@gmail.com>
2016-08-30 23:17:32 -0300

    start poc

---
+++ list_for_of/src/app.js
@@ -0,0 +1,11 @@
+var list = ["test",34,{name:"Guilherme"}];
+
+for (var i in list){
+    console.log(i);
+    console.log(list[i]);
+}
+
+for(let i of list){
+    console.log(i);
+    // console.log(list[i]);
+}
